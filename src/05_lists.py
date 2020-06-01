b49d36364e4c1c520c3af56bbb7b86b79cf1feaa
YasirHasn9 <hassanyasir223@gmail.com>
2020-06-01 12:51:46 -0700

    lists

--- src/05_lists.py
+++ src/05_lists.py
@@ -1,31 +1,39 @@
 # For the exercise, look up the methods and functions that are available for use
 # with Python lists.
 
-x = [1, 2, 3 , 4]
+x = [1, 2, 3]
 y = [8, 9, 10]
 
 # For the following, DO NOT USE AN ASSIGNMENT (=).
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
 x.append(4)
-print(x)
+print("add 4", x)
 
-name = [name for name in names ]
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
-print(x)
+x.extend(y)
+
+print("extend x ", x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
-print(x)
+
+
+x.remove(8)
+print("remove 8 ", x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
-print(x)
+x.insert(5, 99)
+print("insert an element at certain index", x)
 
 # Print the length of list x
 # YOUR CODE HERE
-
+print("length", len(x))
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+for num in x:
+    num *= 100
+    print("Number is multiplied by 1000 ", num)
