2cde40cfdc4a7bc7d013d1a9c4f0de7709743a42
YasirHasn9 <hassanyasir223@gmail.com>
2020-06-01 10:07:54 -0700

    lists

--- guide.py
+++ guide.py
@@ -1,2 +1,17 @@
-import os 
-print(f'hello {os.getlogin()}') ; print("ok")
+# import os 
+# print(f'hello {os.getlogin()}') ; print("ok")
+
+
+# Lists 
+# there is no keyword in python 
+p = []
+q = [1,2,3,4,5]
+
+# add elements to list 
+q.append(6)
+print(q) # add the ele to the end of the list
+
+# to add an el to the beginning of the list insert()
+# list.insert(index = 0 in our case but the future ,  and the ele)
+q.insert(0 , 0)
+print(q)
