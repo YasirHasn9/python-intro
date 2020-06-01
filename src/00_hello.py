c272c696f31fa411bfaa602d7349e85f272861d9
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-31 21:08:11 -0700

    datatypes

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,10 +1,2 @@
 # Print "Hello, world!" to your terminal
 print("hello world")
-
-name = "Joe"
-numbers = [10,20,30]
-numbers.append(40)
-numbers.insert(3,50)
-
-for num in range(0,len(numbers)):
-    print(numbers[num])
